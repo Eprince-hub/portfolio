51df7b1f8a5766b3dfd8282cb75472c2356c8c17
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-06 17:45:50 +0200

    updated the contact me page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,29 +1,34 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Navbar from '../components/Navbar';
-import Head from 'next/head'
 
 function Contact() {
 	return (
 		<>
 			<Head>
-			  <title>Contact</title>
+				<title>Contact</title>
 			</Head>
 
 			<Navbar />
 
-			<section className="hero">
-			  <div className="container">
-			    <div className="text-wrapper w-full">
-			      <h1 className="title">Contact</h1>
-			      <p className="description">
-			      	Incididunt in cillum magna occaecat nisi qui in exercitation labore anim adipisicing amet irure tempor.
-			      </p>
-			      <ul className="contact-links">
-			      	<li className="contact-item">Email: lutfhi@me.com</li>
-			      	<li className="contact-item">Phone: +6212344321007</li>
-			      </ul>
-		      </div>
-	      </div>
+			<section className='hero'>
+				<div className='container'>
+					<div className='text-wrapper w-full'>
+						<h1 className='title'>Contact</h1>
+						<p className='description'>
+							I am actively looking to work in an environment that will give me
+							the opportunity to explore the better version of myself, learn and
+							grow in my career, I am also interested in collaboration and
+							working together with interesting people. So if you have anything
+							for me or an interesting projects for collaboration, please hit me
+							up.
+						</p>
+						<ul className='contact-links'>
+							<li className='contact-item'>Email: ejikello123@yahoo.com</li>
+							<li className='contact-item'>Phone: +436645156126</li>
+						</ul>
+					</div>
+				</div>
 			</section>
 		</>
 	);
